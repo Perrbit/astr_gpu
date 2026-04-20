41a157b7cb8d53daf74495c54715de64dc64b856
perrbit <perrbit@gmail.com>
2026-04-20 21:20:09 +0800

    update vtkio

--- miniapps/tgv_solver_3d/src_gpu/vtkio.F90
+++ miniapps/tgv_solver_3d/src_gpu/vtkio.F90
@@ -1,113 +1,121 @@
 module vtkio
-  use commvar, only: im,jm,km,hm
-  use commarray, only:dx,dy,dz
-  implicit none
+   use commvar, only: im, jm, km, hm
+   use commarray, only: dx, dy, dz
+   implicit none
 
 contains
 
-  subroutine write_vtk_binary(filename, rho, vel, prs, tmp)
-    implicit none
-    character(len=*), intent(in) :: filename
-    real(8), intent(in) :: rho(-hm:im+hm,-hm:jm+hm,-hm:km+hm)
-    real(8), intent(in) :: vel(-hm:im+hm,-hm:jm+hm,-hm:km+hm,1:3)
-    real(8), intent(in) :: prs(-hm:im+hm,-hm:jm+hm,-hm:km+hm)
-    real(8), intent(in) :: tmp(-hm:im+hm,-hm:jm+hm,-hm:km+hm)
-
-    integer :: i,j,k
-    integer :: nx,ny,nz
-    integer :: unit
-    character(len=200) :: line
-
-    nx = im + 1
-    ny = jm + 1
-    nz = km + 1
-
-    open(newunit=unit, file=filename, access='stream', form='unformatted', &
-         status='replace', convert='big_endian')
-
-    write(unit) '# vtk DataFile Version 3.0'//char(10)
-    write(unit) 'CFD binary output'//char(10)
-    write(unit) 'BINARY'//char(10)
-    write(unit) 'DATASET STRUCTURED_POINTS'//char(10)
-
-    ! ===== DIMENSIONS =====
-    write(line,'(A,3(I0,1X))') 'DIMENSIONS ', nx, ny, nz
-    write(unit) trim(line)//char(10)
-
-    ! ===== ORIGIN =====
-    write(unit) 'ORIGIN 0.0 0.0 0.0'//char(10)
-
-    ! ===== SPACING =====
-    write(line,'(A,3(F15.10,1X))') 'SPACING ', dx, dy, dz
-    write(unit) trim(line)//char(10)
-
-    ! ===== POINT_DATA =====
-    write(line,'(A,I0)') 'POINT_DATA ', nx*ny*nz
-    write(unit) trim(line)//char(10)
-
-    ! =========================
-    ! density
-    ! =========================
-    write(unit) 'SCALARS density double'//char(10)
-    write(unit) 'LOOKUP_TABLE default'//char(10)
-
-    do k=0,km
-      do j=0,jm
-        write(unit) (rho(i,j,k), i=0,im)
-      end do
-    end do
-    write(unit) char(10)
-
-    ! =========================
-    ! pressure
-    ! =========================
-    write(unit) 'SCALARS pressure double'//char(10)
-    write(unit) 'LOOKUP_TABLE default'//char(10)
-
-    do k=0,km
-      do j=0,jm
-        write(unit) (prs(i,j,k), i=0,im)
-      end do
-    end do
-    write(unit) char(10)
-
-    ! =========================
-    ! temperature
-    ! =========================
-    write(unit) 'SCALARS temperature double'//char(10)
-    write(unit) 'LOOKUP_TABLE default'//char(10)
-
-    do k=0,km
-      do j=0,jm
-        write(unit) (tmp(i,j,k), i=0,im)
-      end do
-    end do
-    write(unit) char(10)
-
-    ! =========================
-    ! velocity
-    ! =========================
-    write(unit) 'VECTORS velocity double'//char(10)
-
-    do k=0,km
-      do j=0,jm
-        do i=0,im
-          write(unit) vel(i,j,k,1), vel(i,j,k,2), vel(i,j,k,3)
-        end do
-      end do
-    end do
-
-    close(unit)
-
-  end subroutine write_vtk_binary
-
-  subroutine write_vtk_ascii(filename, rho, vel, prs, tmp)
+   subroutine write_vtk_binary(filename, rho, vel, prs, tmp)
       implicit none
       character(len=*), intent(in) :: filename
-      real(8), intent(in) :: rho(-hm:im + hm, -hm:jm + hm, -hm:km + hm)
-      real(8), intent(in) :: vel(-hm:im + hm, -hm:jm + hm, -hm:km + hm, 1:3)
-      real(8), intent(in) :: prs(-hm:im + hm, -hm:jm + hm, -hm:km + hm)
-      real(8), intent(in) :: tmp(-hm:im + hm, -hm:jm + hm, -hm:km + hm)
+      real(8), intent(in), optional :: rho(-hm:im + hm, -hm:jm + hm, -hm:km + hm)
+      real(8), intent(in), optional :: vel(-hm:im + hm, -hm:jm + hm, -hm:km + hm, 1:3)
+      real(8), intent(in), optional :: prs(-hm:im + hm, -hm:jm + hm, -hm:km + hm)
+      real(8), intent(in), optional :: tmp(-hm:im + hm, -hm:jm + hm, -hm:km + hm)
+
+      integer :: i, j, k
+      integer :: nx, ny, nz
+      integer :: unit
+      character(len=200) :: line
+
+      nx = im + 1
+      ny = jm + 1
+      nz = km + 1
+
+      open (newunit=unit, file=filename, access='stream', form='unformatted', &
+            status='replace', convert='big_endian')
+
+      write (unit) '# vtk DataFile Version 3.0'//char(10)
+      write (unit) 'CFD binary output'//char(10)
+      write (unit) 'BINARY'//char(10)
+      write (unit) 'DATASET STRUCTURED_POINTS'//char(10)
+
+      ! ===== DIMENSIONS =====
+      write (line, '(A,3(I0,1X))') 'DIMENSIONS ', nx, ny, nz
+      write (unit) trim(line)//char(10)
+
+      ! ===== ORIGIN =====
+      write (unit) 'ORIGIN 0.0 0.0 0.0'//char(10)
+
+      ! ===== SPACING =====
+      write (line, '(A,3(F15.10,1X))') 'SPACING ', dx, dy, dz
+      write (unit) trim(line)//char(10)
+
+      ! ===== POINT_DATA =====
+      write (line, '(A,I0)') 'POINT_DATA ', nx*ny*nz
+      write (unit) trim(line)//char(10)
+
+      if (present(rho)) then
+         ! =========================
+         ! density
+         ! =========================
+         write (unit) 'SCALARS density double'//char(10)
+         write (unit) 'LOOKUP_TABLE default'//char(10)
+
+         do k = 0, km
+            do j = 0, jm
+               write (unit) (rho(i, j, k), i=0, im)
+            end do
+         end do
+         write (unit) char(10)
+      end if
+
+      if (present(prs)) then
+         ! =========================
+         ! pressure
+         ! =========================
+         write (unit) 'SCALARS pressure double'//char(10)
+         write (unit) 'LOOKUP_TABLE default'//char(10)
+
+         do k = 0, km
+            do j = 0, jm
+               write (unit) (prs(i, j, k), i=0, im)
+            end do
+         end do
+         write (unit) char(10)
+      end if
+
+      if (present(tmp)) then
+         ! =========================
+         ! temperature
+         ! =========================
+         write (unit) 'SCALARS temperature double'//char(10)
+         write (unit) 'LOOKUP_TABLE default'//char(10)
+
+         do k = 0, km
+            do j = 0, jm
+               write (unit) (tmp(i, j, k), i=0, im)
+            end do
+         end do
+         write (unit) char(10)
+      end if
+
+      if (present(vel)) then
+         ! =========================
+         ! velocity
+         ! =========================
+         write (unit) 'VECTORS velocity double'//char(10)
+
+         do k = 0, km
+            do j = 0, jm
+               do i = 0, im
+                  write (unit) vel(i, j, k, 1), vel(i, j, k, 2), vel(i, j, k, 3)
+               end do
+            end do
+         end do
+      end if
+
+      close (unit)
+
+   end subroutine write_vtk_binary
+
+   subroutine write_vtk_ascii(filename, rho, vel, prs, tmp)
+      implicit none
+      character(len=*), intent(in) :: filename
+      real(8), intent(in), optional :: rho(-hm:im + hm, -hm:jm + hm, -hm:km + hm)
+      real(8), intent(in), optional :: vel(-hm:im + hm, -hm:jm + hm, -hm:km + hm, 1:3)
+      real(8), intent(in), optional :: prs(-hm:im + hm, -hm:jm + hm, -hm:km + hm)
+      real(8), intent(in), optional :: tmp(-hm:im + hm, -hm:jm + hm, -hm:km + hm)
 
       integer :: i, j, k, m
       integer :: nx, ny, nz
@@ -139,58 +147,66 @@ contains
       write (line, '(A,I0)') 'POINT_DATA ', nx*ny*nz
       write (unit, '(A)') trim(line)
 
-      ! =========================
-      ! density
-      ! =========================
-      write (unit, '(A)') 'SCALARS density double'
-      write (unit, '(A)') 'LOOKUP_TABLE default'
+      if (present(rho)) then
+         ! =========================
+         ! density
+         ! =========================
+         write (unit, '(A)') 'SCALARS density double'
+         write (unit, '(A)') 'LOOKUP_TABLE default'
 
-      do k = 0, km
-         do j = 0, jm
-            write (unit, '(*(ES25.15E3,1X))') (rho(i, j, k), i=0, im)
+         do k = 0, km
+            do j = 0, jm
+               write (unit, '(*(ES25.15E3,1X))') (rho(i, j, k), i=0, im)
+            end do
          end do
-      end do
+      end if
 
-      ! =========================
-      ! pressure
-      ! =========================
-      write (unit, '(A)') 'SCALARS pressure double'
-      write (unit, '(A)') 'LOOKUP_TABLE default'
+      if (present(prs)) then
+         ! =========================
+         ! pressure
+         ! =========================
+         write (unit, '(A)') 'SCALARS pressure double'
+         write (unit, '(A)') 'LOOKUP_TABLE default'
 
-      do k = 0, km
-         do j = 0, jm
-            write (unit, '(*(ES25.15E3,1X))') (prs(i, j, k), i=0, im)
+         do k = 0, km
+            do j = 0, jm
+               write (unit, '(*(ES25.15E3,1X))') (prs(i, j, k), i=0, im)
+            end do
          end do
-      end do
+      end if
 
-      ! =========================
-      ! temperature
-      ! =========================
-      write (unit, '(A)') 'SCALARS temperature double'
-      write (unit, '(A)') 'LOOKUP_TABLE default'
+      if (present(tmp)) then
+         ! =========================
+         ! temperature
+         ! =========================
+         write (unit, '(A)') 'SCALARS temperature double'
+         write (unit, '(A)') 'LOOKUP_TABLE default'
 
-      do k = 0, km
-         do j = 0, jm
-            write (unit, '(*(ES25.15E3,1X))') (tmp(i, j, k), i=0, im)
+         do k = 0, km
+            do j = 0, jm
+               write (unit, '(*(ES25.15E3,1X))') (tmp(i, j, k), i=0, im)
+            end do
          end do
-      end do
+      end if
 
-      ! =========================
-      ! velocity
-      ! =========================
-      write (unit, '(A)') 'VECTORS velocity double'
+      if (present(vel)) then
+         ! =========================
+         ! velocity
+         ! =========================
+         write (unit, '(A)') 'VECTORS velocity double'
 
-      do k = 0, km
-         do j = 0, jm
-            write (unit, '(*(ES25.15E3,1X))') ((vel(i, j, k, m), m=1, 3), i=0, im)
+         do k = 0, km
+            do j = 0, jm
+               write (unit, '(*(ES25.15E3,1X))') ((vel(i, j, k, m), m=1, 3), i=0, im)
+            end do
          end do
-      end do
-
+      end if
+      
       close (unit)
 
-  end subroutine write_vtk_ascii
+   end subroutine write_vtk_ascii
 
-  subroutine read_vtk_binary(filename, rho, vel, prs, tmp)
+   subroutine read_vtk_binary(filename, rho, vel, prs, tmp)
       implicit none
       character(len=*), intent(in) :: filename
       real(8), intent(out) :: rho(0:im, 0:jm, 0:km)
@@ -297,10 +313,10 @@ contains
 
       close (unit)
 
-  end subroutine read_vtk_binary
+   end subroutine read_vtk_binary
 
 ! Helper subroutine to read a line from binary stream file
-  subroutine read_line(unit, line)
+   subroutine read_line(unit, line)
       implicit none
       integer, intent(in) :: unit
       character(len=*), intent(out) :: line
@@ -317,6 +333,6 @@ contains
             i = i + 1
          end if
       end do
-  end subroutine read_line
+   end subroutine read_line
 
 end module vtkio
